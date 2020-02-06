7493c43f03ac170170997f4417e71c71fe3307a6
@ahowe442 <howe.abigail@gmail.com>
2020-02-05 21:25:30 -0600

    Added create method that redirect to the event list.  Added th:unless to event template.

--- src/main/java/org/launchcode/codingevents/controllers/EventControllers.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventControllers.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +14,10 @@ import java.util.List;
 @RequestMapping("events")
 public class EventControllers {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code with Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -27,5 +26,12 @@ public class EventControllers {
     @GetMapping("create")
     public String renderCreateForm() {
         return "events/create";
+    }
+
+    //lives at /events/create  (they can because they handle different requests.)
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:"; //redirects back into the root path  (can add /path after the colon to redirect it to another place.
     }
 }
